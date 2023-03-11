7f8fa25c99fb89eb6644d228898a18a30f3e9d38
Rihards <r.tirums@gmail.com>
2023-03-11 13:16:17 +0200

    Finished

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 header("Location: interview.html");
 exit;
-?>
+
--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    $("#add_another").click(function() {
+$(document).ready(function () {
+    $("#add_another").click(function () {
         var last_row = $("tbody tr:last-child");
         var new_row = last_row.clone();
         var last_id = parseInt(last_row.find("input:first").attr("name").split("[")[1].split("]")[0]);
         var new_id = last_id + 1;
 
-        new_row.find("input").each(function(index, element) {
+        new_row.find("input").each(function (index, element) {
             var current_name = $(element).attr("name");
             var new_name = current_name.replace("[" + last_id + "]", "[" + new_id + "]");
             $(element).attr("name", new_name);
@@ -15,14 +15,14 @@ $(document).ready(function() {
         new_row.insertAfter(last_row);
     });
 
-    $("form").submit(function(event) {
+    $("form").submit(function (event) {
         event.preventDefault();
         var form_data = $(this).serialize();
         $.ajax({
             url: "submit.php",
             type: "POST",
             data: form_data,
-            success: function(response) {
+            success: function (response) {
                 $("#ajax_results_here").html(response);
             }
         });
